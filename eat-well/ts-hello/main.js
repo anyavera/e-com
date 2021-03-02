e41e7b0e25f90a8345c8ee1c7f9a355efe4e2ac1
anyavera <neverova.anya@gmail.com>
2021-03-02 01:23:05 -0800

    added ts
    playing with ts

---
+++ eat-well/ts-hello/main.js
@@ -0,0 +1,27 @@
+var a;
+//let count = 5;
+//count = 'a';
+/*function log(message) {
+  console.log(message);
+}
+
+var message = 'hello world';
+log (message);*/
+//var number = 1;
+//let count = 2;
+/*function doSomithing () {
+  for (let i = 0; i < 5; i++){
+    console.log(i);
+  }
+  console.log('Finally: ' + i);
+}
+
+doSomithing();*/
+/*function doSomithing () {
+  for (var i = 0; i < 5; i++){
+    console.log(i);
+  }
+  console.log('Finally: ' + i);
+}
+
+doSomithing();*/
---
+++ eat-well/ts-hello/main.ts
@@ -0,0 +1,51 @@
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, 'a', false];
+
+
+
+
+/*let a: number;
+a = 1;
+a = true;
+a = 'a';
+
+console.log(a);*/
+
+
+//let count = 5;
+//count = 'a';
+
+
+
+
+/*function log(message) {
+  console.log(message);
+}
+
+var message = 'hello world';
+log (message);*/
+
+//var number = 1;
+//let count = 2;
+
+/*function doSomithing () {
+  for (let i = 0; i < 5; i++){
+    console.log(i);
+  }
+  console.log('Finally: ' + i);
+}
+
+doSomithing();*/
+
+/*function doSomithing () {
+  for (var i = 0; i < 5; i++){
+    console.log(i);
+  }
+  console.log('Finally: ' + i);
+}
+
+doSomithing();*/
